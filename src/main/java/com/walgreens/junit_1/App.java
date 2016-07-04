7721ff202f306a4d9eba1afd18642373bf48c284
AmySaari <amy.saari@daugherty.com>
2016-07-04 13:13:08 -0500

    Initial commit.

---
+++ src/main/java/com/walgreens/junit_1/App.java
@@ -0,0 +1,13 @@
+package com.walgreens.junit_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
